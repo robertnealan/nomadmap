6a4b0c67b8a2ec58d8db24a04183807d3079f544
Robert Nealan <robertnealan@gmail.com>
2016-08-22 21:23:33 +0700

    Fixing Sheets call to omit need for oauth

--- app.js
+++ app.js
@@ -36,53 +36,11 @@ function addMarker(location, map, title, content) {
 }
 
 /**
- * Check if current user has authorized this application.
- */
-function checkAuth() {
-  gapi.auth.authorize(
-    {
-      'client_id': CLIENT_ID,
-      'scope': SCOPES.join(' '),
-      'immediate': true
-    }, handleAuthResult);
-}
-
-/**
- * Handle response from authorization server.
- *
- * @param {Object} authResult Authorization result.
- */
-function handleAuthResult(authResult) {
-  var authorizeDiv = document.getElementById('authorize-div');
-  if (authResult && !authResult.error) {
-    // Hide auth UI, then load client library.
-    authorizeDiv.style.display = 'none';
-    loadSheetsApi();
-  } else {
-    // Show auth UI, allowing the user to initiate authorization by
-    // clicking authorize button.
-    authorizeDiv.style.display = 'inline';
-  }
-}
-
-/**
- * Initiate auth flow in response to user clicking authorize button.
- *
- * @param {Event} event Button click event.
- */
-function handleAuthClick(event) {
-  gapi.auth.authorize(
-    {client_id: CLIENT_ID, scope: SCOPES, immediate: false},
-    handleAuthResult);
-  return false;
-}
-
-/**
  * Load Sheets API client library.
  */
 function loadSheetsApi() {
   var discoveryUrl =
-      'https://sheets.googleapis.com/$discovery/rest?version=v4';
+      'https://sheets.googleapis.com/$discovery/rest?version=v4&key=AIzaSyB061FXESp_bD9uzFHZZrvbfMIfBJfljX8';
   gapi.client.load(discoveryUrl).then(loadMarkers);
 }
 
